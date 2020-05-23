b5e4d6c711eec77521367f24b1498918bb7525cb
danilogacevic <gacevic.danilo@gmail.com>
2020-05-23 21:24:13 +0200

    add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,6 +92,34 @@ module.exports = {
 				// defer: false,
 			},
 		},
+		{
+			resolve: "gatsby-plugin-google-tagmanager",
+			options: {
+				id: "GTM-5FPQX7S",
+
+				// Include GTM in development.
+				//
+				// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
+
+				// datalayer to be set before GTM is loaded
+				// should be an object or a function that is executed in the browser
+				//
+				// Defaults to null
+				defaultDataLayer: { platform: "gatsby" },
+
+				// Specify optional GTM environment details.
+				// gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+				// gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+				// dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+				// Name of the event that is triggered
+				// on every Gatsby route change.
+				//
+				// Defaults to gatsby-route-change
+				// routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+			},
+		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
